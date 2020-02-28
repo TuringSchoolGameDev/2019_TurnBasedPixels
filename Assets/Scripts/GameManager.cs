62f15d82bccf7f702c44fc95256a79b3167bf899
zivanec <zorian.ivanec@gmail.com>
2020-02-28 10:38:44 +0200

    * added basic grid and menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public GameObject tilePrefab;
+	public GameObject tileGridParent;
+    void Start()
+    {
+		Debug.Log("Dabar loadinsim sita lygi: " + PersistentData.whichLevelToLoad);
+
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{ 
+				GameObject tile = Instantiate(tilePrefab, tileGridParent.transform);
+
+				Vector3 tmpPosition = tile.transform.position;
+				tmpPosition.x += i;
+				tmpPosition.y += j;
+				tile.transform.position = tmpPosition;
+			}
+		}
+    }
+}
